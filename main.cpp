95c566f84d9f8f5a351dd815cc529d30431ea1e6
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2023-11-10 15:16:28 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,13 +24,13 @@ int main() {
     int bruteForceBins = bruteForce.packItems(sortedItems); // Assuming brute force uses sorted items
     int onlineFirstFitBins = firstFit.packItems(items);
     int onlineNextFitBins = nextFit.packItems(items);
-    std::cout << "Online best fit bin order: " << endl;
+    std::cout << "Online best fit bin order: " << std::endl;
     int onlineBestFitBins = bestFit.packItems(items);
 
     // Execute algorithms using the sorted list for offline algorithms
     int offlineFirstFitBins = firstFit.packItems(sortedItems);
     // Before calling the Best Fit algorithm
-    std::cout << "Offline best fit bin order: " << endl;
+    std::cout << "Offline best fit bin order: " << std::endl;
     int offlineBestFitBins = bestFit.packItems(sortedItems); // Correct list for offline
 
     // Output results
