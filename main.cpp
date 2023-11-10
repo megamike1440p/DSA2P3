475bdbc3f34ed215c6ed47ff1dee6eb3e5aefb8b
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2023-11-10 14:33:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-#include "FileReader.h"
-#include "BruteForce.h"
-#include "FirstFit.h"
-#include "NextFit.h"
-#include "BestFit.h"
-#include <iostream>
-#include <algorithm>
-
 int main() {
     FileReader fileReader;
     std::vector<double> items = fileReader.readItems("items.txt");
@@ -13,12 +5,13 @@ int main() {
     // Sort items for offline algorithms
     std::vector<double> sortedItems(items);
     std::sort(sortedItems.begin(), sortedItems.end(), std::greater<double>());
+
+    // Print out the sorted items
     std::cout << "Sorted items: ";
     for (double item : sortedItems) {
-        std::cout << std::fixed << item << " ";
+        std::cout << std::fixed << std::setprecision(2) << item << " ";
     }
     std::cout << std::endl;
-    
 
     // Instantiate algorithms
     BruteForce bruteForce;
@@ -26,20 +19,22 @@ int main() {
     NextFit nextFit;
     BestFit bestFit;
 
-    // Execute algorithms
-    int bruteForceBins = bruteForce.packItems(sortedItems); // For BruteForce, we can use the sorted list
-    int firstFitBins = firstFit.packItems(items);
-    int nextFitBins = nextFit.packItems(items);
-    int bestFitBins = bestFit.packItems(items);
+    // Execute algorithms using the unsorted list for online algorithms
+    int bruteForceBins = bruteForce.packItems(sortedItems); // Assuming brute force uses sorted items
+    int onlineFirstFitBins = firstFit.packItems(items);
+    int onlineNextFitBins = nextFit.packItems(items);
+    int onlineBestFitBins = bestFit.packItems(items);
+
+    // Execute algorithms using the sorted list for offline algorithms
     int offlineFirstFitBins = firstFit.packItems(sortedItems);
-    int offlineBestFitBins = bestFit.packItems(sortedItems);
+    int offlineBestFitBins = bestFit.packItems(sortedItems); // Correct list for offline
 
     // Output results
     std::cout << "Total items: " << items.size() << std::endl;
     std::cout << "Brute Force Bins: " << bruteForceBins << std::endl;
-    std::cout << "Online First Fit Bins: " << firstFitBins << std::endl;
-    std::cout << "Online Next Fit Bins: " << nextFitBins << std::endl;
-    std::cout << "Online Best Fit Bins: " << bestFitBins << std::endl;
+    std::cout << "Online First Fit Bins: " << onlineFirstFitBins << std::endl;
+    std::cout << "Online Next Fit Bins: " << onlineNextFitBins << std::endl;
+    std::cout << "Online Best Fit Bins: " << onlineBestFitBins << std::endl;
     std::cout << "Offline First Fit Bins: " << offlineFirstFitBins << std::endl;
     std::cout << "Offline Best Fit Bins: " << offlineBestFitBins << std::endl;
 
