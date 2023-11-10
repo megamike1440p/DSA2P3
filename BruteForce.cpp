672542ca1b856e742181bdc77cc8510f253d1923
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2023-11-10 15:55:20 -0600

    Update BruteForce.cpp

--- BruteForce.cpp
+++ BruteForce.cpp
@@ -1,26 +1,35 @@
 #include "BruteForce.h"
-#include "NextFit.h"
-#include <algorithm>
+#include <algorithm> // for std::next_permutation
 
 int BruteForce::packItems(std::vector<double> items) {
-    int minBins = items.size();
-    permute(items, 0, items.size(), minBins);
+    // Sort the items in ascending order to generate permutations correctly
+    std::sort(items.begin(), items.end());
+    int minBins = items.size(); // Initialize with the maximum possible number of bins
+
+    do {
+        // Evaluate the current permutation
+        minBins = std::min(minBins, packPermutation(items));
+    } while (std::next_permutation(items.begin(), items.end()));
+
     return minBins;
 }
 
-void BruteForce::permute(std::vector<double>& items, int k, int size, int& minBins) {
-    if (k == size - 1) {
-        minBins = std::min(minBins, packWithNextFit(items));
-    } else {
-        for (int i = k; i < size; i++) {
-            std::swap(items[k], items[i]);
-            permute(items, k + 1, size, minBins);
-            std::swap(items[k], items[i]);
+int BruteForce::packPermutation(std::vector<double>& items) {
+    std::vector<double> bins;
+    for (double item : items) {
+        bool placed = false;
+        for (double &bin : bins) {
+            if (bin + item <= 1.0) {
+                // Place the item in the bin
+                bin += item;
+                placed = true;
+                break;
+            }
+        }
+        if (!placed) {
+            // If the item was not placed in any bin, create a new bin
+            bins.push_back(item);
         }
     }
-}
-
-int BruteForce::packWithNextFit(const std::vector<double>& items) {
-    NextFit nextFitAlgorithm;
-    return nextFitAlgorithm.packItems(items);
+    return bins.size();
 }
