d43ff6b77f588308c0cadce58e5de6ee558e9e9c
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2023-11-10 14:56:49 -0600

    Update BestFit.cpp

--- BestFit.cpp
+++ BestFit.cpp
@@ -6,30 +6,40 @@ int BestFit::packItems(const std::vector<double>& items) {
 
     for (double item : items) {
         int bestBinIndex = -1;
-        double minSpaceLeft = 1.0; // Start with the maximum space a bin can have.
+        double minSpaceLeft = 1.0;
 
         for (size_t i = 0; i < bins.size(); ++i) {
-    double spaceLeft = 1.0 - bins[i].getCurrentSize();
-    // Check for an exact fit first
-    if (item == spaceLeft) {
-        bestBinIndex = i;
-        break; // Break out of the loop as we've found the perfect bin
-    }
-    // Otherwise, find the bin with the least space left after placement
-    else if (item < spaceLeft && spaceLeft < minSpaceLeft) {
-        minSpaceLeft = spaceLeft;
-        bestBinIndex = i;
-    }
-}
+            double spaceLeft = 1.0 - bins[i].getCurrentSize();
+            // Check for an exact fit
+            if (item == spaceLeft) {
+                bestBinIndex = i;
+                break; // Found the perfect fit, no need to look further
+            }
+            else if (item < spaceLeft && spaceLeft - item < minSpaceLeft) {
+                minSpaceLeft = spaceLeft - item;
+                bestBinIndex = i;
+            }
+        }
 
-        // If a suitable bin is found, place the item there.
+        // If a suitable bin is found, place the item there
         if (bestBinIndex != -1) {
             bins[bestBinIndex].addItem(item);
         } else {
-            // Otherwise, create a new bin for the item.
+            // Otherwise, create a new bin for the item
             Bin newBin;
             newBin.addItem(item);
             bins.push_back(newBin);
+        }
+
+        // Debug output for each item placement
+        std::cout << "Item " << item << " placed in bin " 
+                  << (bestBinIndex != -1 ? bestBinIndex : bins.size() - 1) << std::endl;
+        for (size_t i = 0; i < bins.size(); ++i) {
+            std::cout << "Bin " << i << " contains: ";
+            for (double binItem : bins[i].getItems()) {
+                std::cout << binItem << " ";
+            }
+            std::cout << std::endl;
         }
     }
 
