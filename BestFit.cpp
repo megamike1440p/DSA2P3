8712cdbed67b088c2b5fd068e4a4088ad486a9d5
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2023-11-10 14:08:32 -0600

    Update BestFit.cpp

--- BestFit.cpp
+++ BestFit.cpp
@@ -1,31 +1,33 @@
 #include "BestFit.h"
-#include "Bin.h"
-#include <vector>
-#include <algorithm>
 
 int BestFit::packItems(const std::vector<double>& items) {
     std::vector<Bin> bins;
 
+    // Iterate over each item to place it in the best bin
     for (double item : items) {
         int bestBinIndex = -1;
-        double bestFit = 1.0;
+        double minSpaceLeft = 1.0;
 
+        // Find the bin that after placing the item will have the least space left
         for (size_t i = 0; i < bins.size(); ++i) {
             double spaceLeft = 1.0 - bins[i].getCurrentSize();
-            if (item <= spaceLeft && spaceLeft < bestFit) {
-                bestFit = spaceLeft;
+            if (item <= spaceLeft && spaceLeft < minSpaceLeft) {
+                minSpaceLeft = spaceLeft;
                 bestBinIndex = i;
             }
         }
 
+        // If we found a suitable bin, place the item there
         if (bestBinIndex != -1) {
             bins[bestBinIndex].addItem(item);
         } else {
+            // Otherwise, create a new bin for the item
             Bin newBin;
             newBin.addItem(item);
             bins.push_back(newBin);
         }
     }
 
+    // Return the number of bins used
     return bins.size();
 }
