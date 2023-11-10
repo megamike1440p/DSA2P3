02dffc5d76005ecffc683f1dd8283b5d170098e5
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2023-11-10 16:10:00 -0600

    Update BruteForce.cpp

--- BruteForce.cpp
+++ BruteForce.cpp
@@ -4,36 +4,29 @@
 #include <iomanip>   // for std::setprecision
 
 int BruteForce::packItems(std::vector<double> items) {
-    // Sort the items in ascending order to generate permutations correctly
     std::sort(items.begin(), items.end());
-    int minBins = items.size(); // Initialize with the maximum possible number of bins
-    std::vector<std::vector<double>> bestConfiguration;
+    int minBins = items.size(); // Start with the maximum possible bins
 
     do {
         std::vector<Bin> currentBins;
         int currentBinCount = packPermutation(items, currentBins);
         if (currentBinCount < minBins) {
             minBins = currentBinCount;
-            bestConfiguration.clear();
-            for (const Bin& bin : currentBins) {
-                bestConfiguration.push_back(bin.getItems());
-            }
+            bestConfiguration = currentBins; // Update the best configuration
         }
     } while (std::next_permutation(items.begin(), items.end()));
-
-    //Print the best bin configuration
     for (size_t i = 0; i < bestConfiguration.size(); ++i) {
-        std::cout << "\tBrute Force Bin " << i << ": ";
-        for (double item : bestConfiguration[i]) {
-            std::cout << std::fixed << std::setprecision(2) << item << " ";
+        std::cout << "\tBin " << i << ": ";
+        for (double binItem : bestConfiguration[i].getItems()) {
+            std::cout << std::fixed << std::setprecision(2) << binItem << " ";
         }
         std::cout << std::endl;
     }
-
     return minBins;
 }
 
-int BruteForce::packPermutation(const std::vector<double>& items, std::vector<Bin>& bins) {
+int BruteForce::packPermutation(std::vector<double>& items) {
+    std::vector<Bin> bins;
     for (double item : items) {
         bool placed = false;
         for (Bin& bin : bins) {
@@ -43,18 +36,9 @@ int BruteForce::packPermutation(const std::vector<double>& items, std::vector<Bi
             }
         }
         if (!placed) {
-            Bin newBin;
-            newBin.addItem(item);
-            bins.push_back(newBin);
+            bins.emplace_back();
+            bins.back().addItem(item);
         }
     }
-// for (size_t i = 0; i < bins.size(); ++i) {
-//             std::cout << "\tBin " << i << ": ";
-//             for (double binItem : bins[i].getItems()) {
-//                 std::cout << std::fixed << std::setprecision(2) << binItem << " ";
-//             }
-//             std::cout << std::endl;
-//         }
-    
     return bins.size();
 }
